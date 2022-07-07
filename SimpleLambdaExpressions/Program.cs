3e5cfab30586c101a63e8b0801d444fb042b52c2
Sergey Misko <spy_3462@mail.ru>
2022-07-07 16:57:03 +0400

    добавил 2 метода TraditionalDelegateSyntax и IsEvenNumber (цель для делегата Predicate<>)

--- SimpleLambdaExpressions/Program.cs
+++ SimpleLambdaExpressions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SimpleLambdaExpressions
 {
@@ -8,5 +9,21 @@ namespace SimpleLambdaExpressions
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void TraditionalDelegateSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            // Вызвать FindAll() с применением традиционного синтаксиса делегатов.
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = list.FindAll(callback);
+            Console.WriteLine("Here are your even numbers:");
+            foreach(int evenNumber in evenNumbers)
+                Console.WriteLine("{0}\t",evenNumber);
+            Console.WriteLine();
+        }
+
+        // Цель для делегата Predicate<>.
+        static bool IsEvenNumber(int i) => i % 2 == 0;
     }
 }
