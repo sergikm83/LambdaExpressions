405bf4d4754eec6558094c4bf8484d8284ce4279
Sergey Misko <spy_3462@mail.ru>
2022-07-07 17:36:41 +0400

    добавил метод AnonymousMethodSyntax и посмотрел его результаты работы

--- SimpleLambdaExpressions/Program.cs
+++ SimpleLambdaExpressions/Program.cs
@@ -8,7 +8,10 @@ namespace SimpleLambdaExpressions
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Lambdas *****\n");
+            Console.WriteLine("This is TraditionalDelegateSyntax() example:\n");
             TraditionalDelegateSyntax();
+            Console.WriteLine("\nThis is AnonymousMethodSyntax() example:\n");
+            AnonymousMethodSyntax();
         }
 
         static void TraditionalDelegateSyntax()
@@ -26,5 +29,18 @@ namespace SimpleLambdaExpressions
 
         // Цель для делегата Predicate<>.
         static bool IsEvenNumber(int i) => i % 2 == 0;
+
+        static void AnonymousMethodSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            // Теперь использовать анонимный метод.
+            List<int> evenNumbers = list.FindAll(delegate (int i)
+            { return (i % 2 == 0); });
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+                Console.WriteLine("{0}\t", evenNumber);
+            Console.WriteLine();
+        }
     }
 }
