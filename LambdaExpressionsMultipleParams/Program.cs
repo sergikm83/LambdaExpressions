d1661ead3f8d5b79313ed5027ae3b64f478c9866
Sergey Misko <spy_3462@mail.ru>
2022-07-08 15:19:46 +0400

    добавил класс SetMathHandler

--- LambdaExpressionsMultipleParams/Program.cs
+++ LambdaExpressionsMultipleParams/Program.cs
@@ -9,4 +9,18 @@ namespace LambdaExpressionsMultipleParams
             Console.WriteLine("Hello World!");
         }
     }
+    public class SimpleMath
+    {
+        public delegate void MathMessage(string msg, int result);
+        private MathMessage mmDelegate;
+
+        public void SetMathHandler(MathMessage target)
+        {
+            mmDelegate = target;
+        }
+        public void Add(int x, int y)
+        {
+            mmDelegate?.Invoke("Adding has completed!", x + y);
+        }
+    }
 }
